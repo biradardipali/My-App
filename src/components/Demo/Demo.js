c51be9714e221b26e64237182c3b295f80ad3c72
priyank150 <priyank@skills-agency.com>
2019-05-21 16:54:55 +0530

    new_one

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../components/Route/Route';
+
+
+class Demo extends React.Component {
+    render() {
+        return (
+            <h1>React Cheat Sheet</h1>
+        )
+    }
+}
