f6a9be9fd9222aad688975392a27d6a9ff28d476
priyank150 <priyank@skills-agency.com>
2019-05-28 13:43:21 +0530

    new_one_28

--- src/container/Home/home.js
+++ src/container/Home/home.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navigation from '../../components/NavigationBar';
-import videoclip from '../../static/video/2.mp4';
+import videoclip from '../../static/video/1.mp4';
+import Carousel from 'react-bootstrap/Carousel'
+import {card} from 'react-bootstrap'
+import img1 from '../../images/2.png';
+import Carousel1 from 'nuka-carousel';
+import { Slide } from 'react-slideshow-image';
 
+const slideImages = [
+    {img1},
+    {img1},
+    {img1}
+];
 
+const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true
+}
 
 export default class Home extends React.Component {
 
@@ -15,22 +32,76 @@ export default class Home extends React.Component {
                 <div style={{
                     width: '100%',
                     height: '100%',
-                    overflow: 'hidden',
-                    marginTop:'-14.6%'
+                    overflow: 'hidden'
                 }}>
-                    <video id="background-video" loop autoPlay
-                        preload="auto" style={{
+                    <video id="background-video"  autoPlay
+                        style={{
                             objectFit: 'cover',
                             width: '100%',
-                            height: '100%',
+                            height: '625px',
                         }}>
                         <source src={videoclip} type="video/mp4" />
 
                     </video>
-                    <div className="container">
 
+
+                    <div className="contanier" id="welcome">
+                        <div class="col-sm-12" id="welcome_div">
+                            <div className="cards cards_div" >
+                                {/* <h3>Hello..!!!</h3> */}
+                                <Slide {...properties}>
+                                    <div className="each-slide">
+                                        <div style={{ 'backgroundImage': `url(${slideImages[0]})` }}>
+                                        
+                                        <card style={{'text-align' : 'left'}}>
+                                            <h3 style={{'text-align' : 'center','background': '#80808047'}}>Welcome</h3><br></br>
+                                            <p>Life isn't about finding yourself. Life is about creating yourself.</p>
+                                            <p>The most important thing is to enjoy your life - to be happy - it's all that matters.</p>
+                                            <p>I have found that if you love life, life will love you back.</p>
+                                            
+                                        </card>
+                                        </div>
+                                    </div>
+                                    <div className="each-slide">
+                                        <div style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
+                                        <card style={{'text-align' : 'left'}}>
+                                            <h3 style={{'text-align' : 'center', 'background': '#80808047'}}>Welcome</h3><br></br>
+                                            <p>Life isn't about finding yourself. Life is about creating yourself.</p>
+                                            <p>The most important thing is to enjoy your life - to be happy - it's all that matters.</p>
+                                            <p>I have found that if you love life, life will love you back.</p>
+                                           
+                                        </card>
+                                        </div>
+                                    </div>
+                                    <div className="each-slide">
+                                        <div style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
+                                        <card style={{'text-align' : 'left'}}>
+                                            <h3 style={{'text-align' : 'center','background': '#80808047'}}>Welcome</h3><br></br>
+                                            <p>Life isn't about finding yourself. Life is about creating yourself.</p>
+                                            <p>The most important thing is to enjoy your life - to be happy - it's all that matters.</p>
+                                            <p>I have found that if you love life, life will love you back.</p>
+                                           
+                                        </card>
+                                        </div>
+                                    </div>
+                                </Slide>
+                            </div>
+                        </div>
                     </div>
+
+
                 </div>
+                {/* <div className="contanier" id="overlap">
+                        <Carousel1>
+                            <img src={img1} />
+                            <img src={img1} />
+                            <img src={img1} />
+                            <img src={img1} />
+                            <img src={img1} />
+                            <img src={img1} />
+                        </Carousel1>
+                    </div>      */}
+
             </div>
 
         );
